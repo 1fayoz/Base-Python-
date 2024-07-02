80e27f114282bec43ef490f2072b81c08b5cc60a
1fayoz <turaqulovfayoz4@gmail.com>
2024-07-02 15:31:01 +0500

    new

--- clean_code.py
+++ clean_code.py
@@ -1,19 +1,19 @@
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-# Data
-market_types = ['Traditional', 'Modern']
-avg_prices = [83333, 90000]  # Average prices calculated above
+# # Data
+# market_types = ['Traditional', 'Modern']
+# avg_prices = [83333, 90000]  # Average prices calculated above
 
-# Plotting
-plt.figure(figsize=(8, 5))
-plt.bar(market_types, avg_prices, color=['blue', 'orange'])
-plt.title('Average Prices of Lamps in Traditional and Modern Markets')
-plt.xlabel('Market Type')
-plt.ylabel('Average Price (sum)')
-plt.ylim(0, max(avg_prices) * 1.1)  # Set y-axis limit to 10% more than the maximum average price
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.show()
+# # Plotting
+# plt.figure(figsize=(8, 5))
+# plt.bar(market_types, avg_prices, color=['blue', 'orange'])
+# plt.title('Average Prices of Lamps in Traditional and Modern Markets')
+# plt.xlabel('Market Type')
+# plt.ylabel('Average Price (sum)')
+# plt.ylim(0, max(avg_prices) * 1.1)  # Set y-axis limit to 10% more than the maximum average price
+# plt.grid(axis='y', linestyle='--', alpha=0.7)
+# plt.show()
 
 
 
--- leed_code.py
+++ leed_code.py
@@ -271,14 +271,14 @@ print(result)
 #        number += translations[char]
 #        return number
 
-def should_i_wear_this_hat(self,hat):
-    if not isinstance(hat, Hat):
-        return False
-    current_fashion = get_fashion()
-    weather_outside = self.look_out_of_window()
-    is_stylish = self.evaluate_style(hat, current_fashion)
-    if weather_outside.is_raining:
-        print()
+# def should_i_wear_this_hat(self,hat):
+#     if not isinstance(hat, Hat):
+#         return False
+#     current_fashion = get_fashion()
+#     weather_outside = self.look_out_of_window()
+#     is_stylish = self.evaluate_style(hat, current_fashion)
+#     if weather_outside.is_raining:
+#         print()
 
 
 
